1762bdc0f0d5ad1076a14e8a1d97c668c9e15a42
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-04-05 18:41:03 -0300

    Create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :notes, only: [:create, :update, :index, :show, :destroy]
+
+  post '/users/:id/add_note/:note_id', to: 'users#add_note'
+  delete '/users/:id/remove_note/:note_id', to: 'users#remove_note'  
 end
